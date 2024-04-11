a6839614aec71058379eabd27749612f873eb172
MandavkarPranjal <pranjalmandavkar10@gmail.com>
2024-04-11 23:58:29 +0530

    added rust_analyser and gopls as ensure install in mason-installer

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "biome", "tsserver", "vtsls", "sqlls", "clangd" }
+                ensure_installed = { "lua_ls", "biome", "tsserver", "vtsls", "sqlls", "clangd", "rust_analyzer", "gopls" }
             })
         end
     },
