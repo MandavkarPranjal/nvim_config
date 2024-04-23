721078306b438e81843502fea8b15f7cb4ac9155
MandavkarPranjal <programmer.pranjal@gmail.com>
2024-04-23 13:06:23 +0530

    commented mini.nvim, added lualine.nvim as resplacement for mini.nvim and set theme to gruvbox

--- init.lua
+++ init.lua
@@ -331,6 +331,25 @@ require('lazy').setup({
     end
   },
 
+  { 
+     "morhetz/gruvbox",
+     config = function()
+         vim.cmd("colorscheme gruvbox")
+     end
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup({
+        options = {
+        theme = 'dracula'
+      }
+    })
+    end
+  },
+
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
@@ -768,43 +787,43 @@ local function toggle_telescope(harpoon_files)
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
+  -- { -- Collection of various small independent plugins/modules
+    -- 'echasnovski/mini.nvim',
+    -- config = function()
+      -- -- Better Around/Inside textobjects
+      -- --
+      -- -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      -- --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      -- --  - ci'  - [C]hange [I]nside [']quote
+      -- require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- --
+      -- -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- -- - sd'   - [S]urround [D]elete [']quotes
+      -- -- - sr)'  - [S]urround [R]eplace [)] [']
+      -- require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- -- Simple and easy statusline.
+      -- --  You could remove this setup call if you don't like it,
+      -- --  and try some other statusline plugin
+      -- local statusline = require 'mini.statusline'
+      -- -- set use_icons to true if you have a Nerd Font
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- -- You can configure sections in the statusline by overriding their
+      -- -- default behavior. For example, here we set the section for
+      -- -- cursor location to LINE:COLUMN
+      -- ---@diagnostic disable-next-line: duplicate-set-field
+      -- statusline.section_location = function()
+        -- return '%2l:%-2v'
+      -- end
 
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
+      -- -- ... and there is more!
+      -- --  Check out: https://github.com/echasnovski/mini.nvim
+    -- end,
+  -- },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
