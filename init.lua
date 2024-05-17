59d8a62e593017a0312b14bb9985ec7033fbb9ee
MandavkarPranjal <programmer.pranjal@gmail.com>
2024-05-17 11:22:45 +0530

    changed keymap for Oil

--- init.lua
+++ init.lua
@@ -355,7 +355,7 @@ require('lazy').setup({
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup()
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
   },
   --This is my config for neo-tree
