3ad13cde78110139149c5df667907bcf21a79454
MandavkarPranjal <programmer.pranjal@gmail.com>
2024-05-18 11:38:33 +0530

    trying different colorscheme

--- init.lua
+++ init.lua
@@ -437,10 +437,22 @@ require('lazy').setup({
     },
   },
 
+  -- { 
+  --   "bluz71/vim-moonfly-colors", 
+  --   name = "moonfly", 
+  --   lazy = false, 
+  --   priority = 1000,
+  --   config = function()
+  --     -- Lua initialization file
+  --     vim.cmd [[colorscheme moonfly]]
+  --   end
+  -- },
   {
     "morhetz/gruvbox",
+    -- priority = 1000,
     config = function()
       vim.cmd("colorscheme gruvbox")
+      -- vim.cmd.hi 'Comment gui=none'
     end
   },
 
@@ -923,23 +935,23 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
