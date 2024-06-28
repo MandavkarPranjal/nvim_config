48b520f7385d53b70ea933bd2f9186c5d73b9365
MandavkarPranjal <programmer.pranjal@gmail.com>
2024-06-28 12:42:55 +0530

    Remove: Neogit

--- init.lua
+++ init.lua
@@ -199,17 +199,6 @@ require('lazy').setup({
   --   end
   -- },
   {
-    "NeogitOrg/neogit",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      -- Only one of these is needed, not both.
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-    config = true
-  },
-  {
     "sindrets/diffview.nvim",
     event = "VeryLazy",
     config = function()
